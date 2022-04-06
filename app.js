5448d838395fba08f29633def9591e27f7a17509
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-04-05 19:28:47 -0700

    lesson 9.3.5 1st inquirer prompt

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('.index.html', pageHTML, err=> {
+//   if (err) throw err;
 
-
-  fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-// const profileDataArgs = process.argv.slice(2);
-
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
-
-// const printProfileData = profileDataArr => {
-    // This.....
-    // for (let i = 0; i < profileDataArr.length; i += 1 ) {
-        // console.log(profileDataArr[i]);
-    // }
-
-    // console.log('================');
-    //  Is the same as this...
-    // profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
